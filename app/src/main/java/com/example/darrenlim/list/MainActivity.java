ebbbb62647e39ea7f744b37addadff1c92badc05
Matthew <mjlease@buffalo.edu>
2015-12-11 19:20:55 -0500

    Systemclocks removed

--- app/src/main/java/com/example/darrenlim/list/MainActivity.java
+++ app/src/main/java/com/example/darrenlim/list/MainActivity.java
@@ -142,7 +142,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         _currentUser = ParseUser.getCurrentUser();
         if(_currentUser!= null) {
             getData();
-            SystemClock.sleep(1000);
         }
         else {
             LayoutInflater inflater = getLayoutInflater();
@@ -252,7 +251,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         });
 
-//        SystemClock.sleep(1000);
     }
 
     public void deleteItemFromCloud(int position) {
@@ -272,12 +270,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         if (requestCode == 1) {
             if(resultCode == Activity.RESULT_OK){
-//                SystemClock.sleep(2000);
-//                getData();
-//                Intent intent = getIntent();
-//                Reminder reminder = (Reminder) intent.getSerializableExtra("Reminder");
-//                _data.add(reminder);
-
                 _rAdapter.updateAdapter(_data);
                 _rAdapter.notifyItemInserted(0);
                 _recyclerView.smoothScrollToPosition(0);
@@ -400,7 +392,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             }
         });
 
-        SystemClock.sleep(2000);
+        SystemClock.sleep(1000);
         ParseUser.logInInBackground(username, password, new LogInCallback() {
             @Override
             public void done(ParseUser user, ParseException e) {
@@ -408,6 +400,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 _dialog.dismiss();
                 _dialog = null;
                 _dialogView = null;
+                getData();
             }
         });
     }
@@ -435,6 +428,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     _dialog.dismiss();
                     _dialog = null;
                     _dialogView = null;
+                    getData();
                 }
                 else {
                     Toast.makeText(getApplicationContext(), R.string.logInError, Toast.LENGTH_LONG).show();
